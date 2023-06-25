b48317a0a0002b2c78978ac21bae0875e0afc127
Fores <draeger.tnt.@yandex.ru>
2023-06-25 21:17:35 +0300

    Тест прошёл успешно!

--- src/Goblin.java
+++ src/Goblin.java
@@ -1,5 +1,6 @@
-public class Goblin extends Monster{
+public class Goblin extends Monster {
+    static Player player;
     public Goblin(String name) {
-        super(name, 100, 10, 25, 10, 20);
+        super(name, 100, 10, 25, 7, ((int) (Math.random() * 50)));
     }
 }
